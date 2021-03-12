2df939469893f40b4b1128591496afc16efe7088
gukyue88 <gukyue88@gmail.com>
2021-03-12 20:04:18 +0900

    hit the url
    Signed-off-by: gukyue88 <gukyue88@gmail.com>

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
 
+var errRequestFailed = errors.New("Request failed")
+
+func main() {
+	urls := []string{
+		"https://www.airbnb.com",
+		"https://www.google.com",
+		"https://www.amazon.com",
+		"https://www.reddit.com",
+		"https://soundcloud.com",
+		"https://www.facebook.com",
+		"https://www.instagram.com",
+		"https://academy.nomadcoders.co",
+		"https://www.daum.net",
+		"https://www.naver.com",
+	}
+
+	for _, url := range urls {
+		fmt.Println("Checking:", url)
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	// go lang standard library 검색!
+	resp, err := http.Get(url)
+	if err == nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+
+	return nil
 }
