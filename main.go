a51c3030168fa8ee0532c5c13a8fedead2e4fb04
gukyue88 <gukyue88@gmail.com>
2021-03-12 17:52:37 +0900

    how to print struct in Go
    Signed-off-by: gukyue88 <gukyue88@gmail.com>

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/comdori-web/learngo/accounts"
 )
@@ -10,12 +9,12 @@ import (
 func main() {
 	account := accounts.NewAccount("kyuha")
 	account.Deposit(10)
-	fmt.Println(account.Balance())
 
 	// go 에서 error를 처리하는 방법임.
 	err := account.Withdraw(20)
 	if err != nil {
-		log.Fatalln(err) // 프로그램을 종료 시켜줌
+		// log.Fatalln(err) // 프로그램을 종료 시켜줌
+		fmt.Println(err)
 	}
-	fmt.Println(account.Balance())
+	fmt.Println(account) // go 가 자동으로 String() 을 호출해주고 있는거임. like python
 }
