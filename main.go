5a33d1ea806e8fe7437518632addbf5d7ddf306d
gukyue88 <gukyue88@gmail.com>
2021-03-12 19:55:33 +0900

    reset main.go
    Signed-off-by: gukyue88 <gukyue88@gmail.com>

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/comdori-web/learngo/mydict"
-)
-
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	err := dictionary.Update(baseWord, "Second")
-	if err != nil {
-		fmt.Println(err)
-	}
 
-	word, _ := dictionary.Search(baseWord)
-	fmt.Println(word)
-
-	fmt.Println(dictionary)
-	dictionary.Delete(baseWord)
-	fmt.Println(dictionary)
 }
