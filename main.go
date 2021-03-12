3c37cb7a502fcb598e5c3b3009449ccaef0c000f
gukyue88 <gukyue88@gmail.com>
2021-03-12 18:32:24 +0900

    Add 'add method'
    Signed-off-by: gukyue88 <gukyue88@gmail.com>

--- main.go
+++ main.go
@@ -8,10 +8,25 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("second")
+	//definition, err := dictionary.Search("second")
+	//if err != nil {
+	//fmt.Println(err)
+	//} else {
+	//fmt.Println(definition)
+	//}
+
+	word := "hello"
+	definition := "Greeting"
+
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
+	}
+
+	hello, _ := dictionary.Search(word)
+	fmt.Println(hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
 	}
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -6,6 +6,7 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That word already exists")
 
 // go 에서는 type도 method를 가질 수 있다!
 
@@ -17,4 +18,17 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 
 	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
 }
