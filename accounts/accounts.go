d7ca4cd167a1ddef25ab187d8861b23f37bae62a
gukyue88 <gukyue88@gmail.com>
2021-03-12 17:42:50 +0900

    Error Handling in Go
    Signed-off-by: gukyue88 <gukyue88@gmail.com>

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,10 +1,14 @@
 package accounts
 
+import "errors"
+
 // Account struct
 type Account struct {
 	owner   string
 	balance int
 }
+
+var errNoMoney = errors.New("Can't withdraw")
 
 // NewAccount creates Account
 func NewAccount(owner string) *Account {
@@ -14,15 +18,30 @@ func NewAccount(owner string) *Account {
 }
 
 // Deposit x amount on your account
-func (a Account) Deposit(amount int) {
+func (a *Account) Deposit(amount int) {
 	// a Account : receiver
 	// receiver로 method를 만들 수 있음
 	// a는 다른 이름이어도 된다.
 	// 대신 struct의 첫 글자를 따서 소문자로 지어야 한다!
 	a.balance += amount
+	// 신기한 것은 포인터로 변경해도 호출부분과 접근 부분의 차이가 없네...
 }
 
 // Balance of your account
 func (a Account) Balance() int {
 	return a.balance
+}
+
+// Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) error {
+	// go에는 exception 같은 것이 없다.
+	// err를 return을 직접 해줘야 한다.
+	if a.balance < amount {
+		//return error.Error()
+		return errNoMoney
+	}
+
+	a.balance -= amount
+
+	return nil
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/comdori-web/learngo/accounts"
 )
@@ -9,5 +10,12 @@ import (
 func main() {
 	account := accounts.NewAccount("kyuha")
 	account.Deposit(10)
+	fmt.Println(account.Balance())
+
+	// go 에서 error를 처리하는 방법임.
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err) // 프로그램을 종료 시켜줌
+	}
 	fmt.Println(account.Balance())
 }
