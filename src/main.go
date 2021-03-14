8790eb1bf87ba64ccc058ca4c5688fd4acc5fa61
comdori.web@gmail.com <gukyue88@gmail.com>
2021-03-14 18:45:26 +0900

    extractJob part Two
    Signed-off-by: comdori.web@gmail.com <gukyue88@gmail.com>

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -14,19 +15,26 @@ type extractedJob struct {
 	title    string
 	location string
 	salary   string
+	summary  string
 }
 
 var baseURL string = "https://kr.indeed.com/jobs?q=python&start="
 
 func main() {
+	var jobs []extractedJob
 	totalPages := getPages()
 	println(totalPages)
 	for i := 0; i < totalPages; i++ {
 		getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...) // 이거 문법은 한 번 더 봐야할듯
 	}
+
+	fmt.Println(jobs)
 }
 
-func getPage(page int) {
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
 	pageURL := baseURL + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageURL)
 	res, err := http.Get(pageURL)
@@ -41,15 +49,30 @@ func getPage(page int) {
 
 	searchCards := doc.Find(".jobsearch-SerpJobCard")
 	searchCards.Each(func(i int, card *goquery.Selection) {
-		id, _ := card.Attr("data-jk")
-		title := card.Find(".title>a").Text()
-		location := card.Find(".sjcl").Text()
-		fmt.Println(id, title, location)
+		job := extractJob(card)
+		jobs = append(jobs, job)
 	})
+
+	return jobs
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("data-jk")
+	title := cleanString(card.Find(".title>a").Text())
+	location := cleanString(card.Find(".sjcl").Text())
+	salary := cleanString(card.Find(".salaryText").Text())
+	summary := cleanString(card.Find(".summary").Text())
+	return extractedJob{
+		id:       id,
+		title:    title,
+		location: location,
+		salary:   salary,
+		summary:  summary,
+	}
 }
 
 func cleanString(str string) string {
-
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
 func getPages() int {
