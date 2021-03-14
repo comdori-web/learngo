101cf18c16cf5576852cb8b6ec2a1b59dde0b909
comdori.web@gmail.com <gukyue88@gmail.com>
2021-03-14 14:18:18 +0900

    getPages part One
    Signed-off-by: comdori.web@gmail.com <gukyue88@gmail.com>

--- src/main.go
+++ src/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/PuerkitoBio/goquery"
+)
+
+var baseURL string = "https://kr.indeed.com/jobs?q=python&start="
+
+func main() {
+	pages := getPages()
+	println(pages)
+}
+
+func getPages() int {
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	// io.Reader 는 Close() 가 필요함!
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	fmt.Println(doc)
+	doc.Find(".pagination")
+
+	return 0
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status : ", res.StatusCode)
+	}
 }
